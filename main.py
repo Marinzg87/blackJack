2e9550104773ee4027c548cb1f222a0ab122d591
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-07-31 08:11:15 +0200

    First version of solution.

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import random
 deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
+def get_cards(hand, number):
+    for card in range(number):
+        hand.append(random.choice(deck))
+    return hand
+
+
 def hand_sum(hand):
     on_hand = 0
     for card in hand:
@@ -11,20 +17,82 @@ def hand_sum(hand):
     return on_hand
 
 
-def first_two_cards(hand):
-    for card in range(2):
-        hand.append(random.choice(deck))
-    return hand
-
-
+# Empty lists of cards for user and computer
 user_hand = []
 computer_hand = []
 
-first_two_cards(user_hand)
-first_two_cards(computer_hand)
+# First two cards for each player
+get_cards(user_hand, 2)
+get_cards(computer_hand, 2)
 
-print(f"User hand: {user_hand}, Computer hand: {computer_hand}")
+computer_hand_view = []
+computer_hand_view.append(computer_hand[0])
+computer_hand_view.append("_")
+print(f"User hand: {user_hand}\nComputer hand: {computer_hand_view}")
 
+# Sum the deck values
 user_sum = hand_sum(user_hand)
 computer_sum = hand_sum(computer_hand)
-print(f"User score: {user_sum}, Computer score: {computer_sum}")
+
+# BlackJack game
+is_blackjack = False
+while not is_blackjack:
+    if user_sum == 21:
+        print("User win!")
+        print(f"User score: {user_sum}, Computer score: {computer_sum}")
+        is_blackjack = True
+        break
+    elif computer_sum == 21:
+        print("Computer win!")
+        print (f"User score: {user_sum}, Computer score: {computer_sum}")
+        is_blackjack = True
+        break
+    elif user_sum > 21:
+        if user_hand.count(11) == 1:
+            user_sum -= 10
+            if user_sum > 21:
+                print("Computer win!")
+                print(f"User score: {user_sum}, Computer score: {computer_sum}")
+                is_blackjack = True
+                break
+            else:
+                if input("Would you like another card? 'y' or 'n'\n") == "y":
+                    get_cards(user_hand, 1)
+                    user_sum = hand_sum(user_hand)
+                    if user_sum > 21:
+                        print("Computer win!")
+                        print(f"User score: {user_sum}, Computer score: {
+                        computer_sum}")
+                        is_blackjack = True
+                        break
+    else:
+        if input("Would you like another card? 'y' or 'n'\n") == "y":
+            get_cards(user_hand, 1)
+            user_sum = hand_sum(user_hand)
+            if user_sum > 21:
+                print("Computer win!")
+                print(f"User score: {user_sum}, Computer score: {computer_sum}")
+                is_blackjack = True
+                break
+    while computer_sum < 17:
+        get_cards(computer_hand, 1)
+        computer_sum = hand_sum(computer_hand)
+        if computer_sum > 21:
+            print("User win!")
+            print(f"User score: {user_sum}, Computer score: {computer_sum}")
+            is_blackjack = True
+            break
+    if user_sum > computer_sum:
+        print("User win!")
+        print(f"User score: {user_sum}, Computer score: {computer_sum}")
+        is_blackjack = True
+        break
+    elif computer_sum > user_sum:
+        print("Computer win!")
+        print(f"User score: {user_sum}, Computer score: {computer_sum}")
+        is_blackjack = True
+        break
+    else:
+        print("Draw")
+        print(f"User score: {user_sum}, Computer score: {computer_sum}")
+        is_blackjack = True
